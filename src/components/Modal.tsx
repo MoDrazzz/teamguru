4d1ba71806239013d77670558330fa1834c98315
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-08-07 18:48:19 +0200

    feat: hide modal on escape or background click

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import Icon from '@/components/Icon'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
-import { Dispatch, PropsWithChildren, SetStateAction } from 'react'
+import { Dispatch, PropsWithChildren, SetStateAction, useEffect } from 'react'
 
 interface Props extends PropsWithChildren {
   isVisible: boolean
@@ -8,10 +10,25 @@ interface Props extends PropsWithChildren {
 }
 
 const Modal = ({ isVisible, setIsVisible, children }: Props) => {
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') setIsVisible(false)
+    }
+
+    document.addEventListener('keydown', handleEscape)
+    return () => document.removeEventListener('keydown', handleEscape)
+  }, [setIsVisible])
+
   return (
     isVisible && (
-      <div className="absolute w-screen h-screen top-0 left-0 grid place-items-center z-40 bg-slate-900 bg-opacity-50">
-        <div className="relative bg-slate-50 rounded-lg p-12">
+      <div
+        className="absolute w-screen h-screen top-0 left-0 grid place-items-center z-40 bg-slate-900 bg-opacity-50"
+        onClick={() => setIsVisible(false)}
+      >
+        <div
+          className="relative bg-slate-50 rounded-lg p-12"
+          onClick={(e) => e.stopPropagation()}
+        >
           <span
             className="absolute right-3 top-3 cursor-pointer"
             onClick={() => setIsVisible(false)}
