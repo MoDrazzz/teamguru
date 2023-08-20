49c6d7ed51e5105b96553554169c67628a4875cf
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-08-20 17:52:24 +0200

    feat: add logout button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,15 @@
 import { Identity, Logo, Navigation } from '@/components'
+import LogoutButton from '@/components/LogoutButton'
 
 const Sidebar = () => {
   return (
-    <aside className="flex w-64 flex-col gap-16 border-r-2 border-slate-400 bg-slate-50 px-6 py-12">
+    <aside className="flex w-64 flex-col gap-12 border-r-2 border-slate-400 bg-slate-50 px-6 py-12">
       <Logo isClickable />
       <Identity />
       <Navigation />
+      <div className="flex h-full flex-col-reverse">
+        <LogoutButton />
+      </div>
     </aside>
   )
 }
