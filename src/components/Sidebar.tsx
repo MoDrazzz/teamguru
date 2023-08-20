dea58e92c5aad5497d3d6244642384495c07ac19
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-08-20 16:08:10 +0200

    fix: bumping on load and wrong spacings

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import { Identity, Logo, Navigation } from '@/components'
 
 const Sidebar = () => {
   return (
-    <aside className="flex w-64 flex-col gap-8 border-r-2 border-slate-400 bg-slate-50 px-6 py-12">
+    <aside className="flex w-64 flex-col gap-16 border-r-2 border-slate-400 bg-slate-50 px-6 py-12">
       <Logo />
       <Identity />
       <Navigation />
--- src/components/Skeletons/IdentitySkeleton.tsx
+++ src/components/Skeletons/IdentitySkeleton.tsx
@@ -2,7 +2,7 @@ const IdentitySkeleton = () => {
   return (
     <div className="grid animate-pulse justify-items-center gap-1">
       <div className="h-4 w-16 rounded-full bg-slate-200" />
-      <div className="h-8 w-full rounded-full bg-slate-200" />
+      <div className="h-7 w-full rounded-full bg-slate-200" />
     </div>
   )
 }
