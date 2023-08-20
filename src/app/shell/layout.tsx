cf17fb1adecd320173881fc4a9a3d79d06d8226b
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-08-20 14:36:37 +0200

    feat: add sidebar core

--- src/app/shell/layout.tsx
+++ src/app/shell/layout.tsx
@@ -1,6 +1,14 @@
+import { Sidebar } from '@/components'
 import { AuthContext } from '@/contexts'
 import { PropsWithChildren } from 'react'
 
 export default function ShellLayout({ children }: PropsWithChildren) {
-  return <AuthContext>{children}</AuthContext>
+  return (
+    <AuthContext>
+      <div className="flex h-full">
+        <Sidebar />
+        {children}
+      </div>
+    </AuthContext>
+  )
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { Logo } from '@/components'
+
+const Sidebar = () => {
+  return (
+    <aside className="flex w-64 flex-col gap-8 border-r-2 border-slate-400 bg-slate-50 px-6 py-12">
+      <Logo />
+    </aside>
+  )
+}
+
+export default Sidebar
