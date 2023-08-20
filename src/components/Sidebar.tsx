1db778799d23ec0e1b2921bf755094cf70c6fa7d
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-08-20 14:44:51 +0200

    feat: add identity component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,10 @@
-import { Logo } from '@/components'
+import { Identity, Logo } from '@/components'
 
 const Sidebar = () => {
   return (
     <aside className="flex w-64 flex-col gap-8 border-r-2 border-slate-400 bg-slate-50 px-6 py-12">
       <Logo />
+      <Identity />
     </aside>
   )
 }
---
+++ src/components/Skeletons/IdentitySkeleton.tsx
@@ -0,0 +1,10 @@
+const IdentitySkeleton = () => {
+  return (
+    <div className="grid animate-pulse justify-items-center gap-1">
+      <div className="h-4 w-16 rounded-full bg-slate-200" />
+      <div className="h-8 w-full rounded-full bg-slate-200" />
+    </div>
+  )
+}
+
+export default IdentitySkeleton
