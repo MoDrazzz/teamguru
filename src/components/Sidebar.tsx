6362413f13c28ad6960f07ab736279268c5a37c8
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-08-20 16:13:18 +0200

    feat: redirect to dashboard on logo click

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Identity, Logo, Navigation } from '@/components'
 const Sidebar = () => {
   return (
     <aside className="flex w-64 flex-col gap-16 border-r-2 border-slate-400 bg-slate-50 px-6 py-12">
-      <Logo />
+      <Logo isClickable />
       <Identity />
       <Navigation />
     </aside>
