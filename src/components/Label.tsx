02125b5f58e01ce31586a963a805e61004772cfc
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-09-15 14:15:20 +0200

    feat: add input and label hover effect

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -8,7 +8,7 @@ const Label = ({ htmlFor, children }: Props) => {
   return (
     <label
       htmlFor={htmlFor}
-      className="text-xs font-semibold text-slate-500 sm:text-sm"
+      className="text-xs font-semibold text-slate-500 group-focus-within:text-slate-600 sm:text-sm"
     >
       {children}
     </label>
