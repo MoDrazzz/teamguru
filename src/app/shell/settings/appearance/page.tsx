313489d5da32eb52ab460d6ca78492eac4dfa150
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-09-18 11:33:34 +0200

    feat: add page heading

--- src/app/shell/settings/appearance/page.tsx
+++ src/app/shell/settings/appearance/page.tsx
@@ -1,3 +1,12 @@
+import { PageHeading } from '@/components'
+
 export default function AppearanceSettings() {
-  return <h1>Settings - Appearance</h1>
+  return (
+    <main className="flex w-full flex-col gap-8 bg-slate-50 p-16">
+      <PageHeading
+        title="Appearance"
+        subtitle="Choose your favourite theme to make the application even better."
+      />
+    </main>
+  )
 }
