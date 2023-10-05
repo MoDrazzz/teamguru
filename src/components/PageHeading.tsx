591aa5d5e5d269b368800d64ee3db4c7949e502a
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-10-05 11:51:49 +0200

    fix: page heading width

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const PageHeading = ({ title, subtitle }: Props) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex w-full flex-col">
       <h1 className="mb-2 text-2xl font-semibold">{title}</h1>
       <h2 className="mb-4 text-slate-600 dark:text-zinc-400">{subtitle}</h2>
       <Spacer />
