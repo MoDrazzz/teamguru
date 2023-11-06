017f0ca6684af93570908b14e359da0757d05b49
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-11-06 13:46:26 +0100

    test: setup vitest

---
+++ src/__tests__/setup.ts
@@ -0,0 +1,5 @@
+import { loadEnvConfig } from '@next/env'
+
+// Setup environmental variables
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/__tests__/setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
