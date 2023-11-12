5c3f2d3942f42326a07b14db5e3494b7ba0c878b
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-11-12 16:29:56 +0100

    feat: allow input to be controlled

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,12 @@
 import classNames from 'classnames'
-import { RefObject } from 'react'
+import { ChangeEvent, RefObject } from 'react'
 
 interface Props {
   type?: 'text' | 'email' | 'password'
-  refObj: RefObject<HTMLInputElement>
+  refObj?: RefObject<HTMLInputElement>
+  value?: string
+  // eslint-disable-next-line no-unused-vars
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void
   placeholder: string
   id?: string
   isError?: boolean
@@ -19,6 +22,8 @@ interface Props {
 const Input = ({
   type = 'text',
   refObj,
+  value,
+  onChange,
   id,
   placeholder,
   isError = false,
@@ -29,12 +34,15 @@ const Input = ({
     <input
       type={type}
       ref={refObj}
+      value={value}
+      onChange={onChange}
       id={id}
       className={classNames(
-        'w-full rounded-lg border-2 bg-slate-50 dark:bg-zinc-900 px-3 py-2 text-xs font-medium outline-none transition-colors sm:text-sm',
+        'w-full rounded-lg border-2 bg-slate-50 px-3 py-2 text-xs font-medium outline-none transition-colors dark:bg-zinc-900 sm:text-sm',
         {
           'border-red-500 text-red-700': isError,
-          'border-slate-400 focus:border-slate-600 dark:border-zinc-500 dark:focus:border-zinc-400': !isError,
+          'border-slate-400 focus:border-slate-600 dark:border-zinc-500 dark:focus:border-zinc-400':
+            !isError,
         },
       )}
       placeholder={placeholder}
