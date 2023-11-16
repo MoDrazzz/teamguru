ea565b17c9b7732e48b3a9a8c0e32589c98c1023
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-11-16 13:10:14 +0100

    feat: add Pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,31 @@
+import { ButtonAlt } from '@/components'
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { Dispatch, SetStateAction } from 'react'
+
+interface Props {
+  page: number
+  setPage: Dispatch<SetStateAction<number>>
+  length: number
+}
+
+const Pagination = ({ page, setPage, length }: Props) => {
+  return (
+    <div className="flex items-center">
+      <ButtonAlt
+        onClick={() => setPage((prev) => prev - 1)}
+        icon={faArrowLeft}
+        disabled={page === 1}
+      />
+      <p className="w-14 text-center text-sm font-medium">
+        {page} / {length}
+      </p>
+      <ButtonAlt
+        onClick={() => setPage((prev) => prev + 1)}
+        icon={faArrowRight}
+        disabled={page === length}
+      />
+    </div>
+  )
+}
+
+export default Pagination
