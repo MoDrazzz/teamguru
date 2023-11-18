c1728bac164fda0ab2ca5a3d92c82c3923aaf4a0
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-11-18 13:25:49 +0100

    feat: extract button icon component

--- src/components/MessageIcon.tsx
+++ src/components/MessageIcon.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { Icon, Tooltip } from '@/components'
+import { ButtonIcon } from '@/components'
 import { UserProfileType } from '@/types'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { useRouter } from 'next/navigation'
-import { useRef } from 'react'
 
 interface Props {
   receiver: UserProfileType['id']
@@ -12,24 +11,18 @@ interface Props {
 
 const MessageIcon = ({ receiver }: Props) => {
   const router = useRouter()
-  const ref = useRef(null)
 
   const handleDirectMessage = () => {
     router.push(`/shell/messages/${receiver}`)
   }
 
   return (
-    <div
-      ref={ref}
+    <ButtonIcon
       onClick={handleDirectMessage}
-      className="group relative flex cursor-pointer p-0.5"
-    >
-      <Icon
-        icon={faEnvelope}
-        className="text-xl text-slate-600 transition-colors group-hover:text-slate-700 dark:text-zinc-400 dark:group-hover:text-zinc-300"
-      />
-      <Tooltip tooltipTargetRef={ref} message="Direct Message" />
-    </div>
+      icon={faEnvelope}
+      tooltipMessage="Direct Message"
+      className="text-xl text-slate-600 transition-colors group-hover:text-slate-700 dark:text-zinc-400 dark:group-hover:text-zinc-300"
+    />
   )
 }
 
